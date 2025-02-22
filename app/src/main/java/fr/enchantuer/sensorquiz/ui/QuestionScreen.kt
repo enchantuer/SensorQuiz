17465eec514fe82f7076857bdc944a83f52046dd
Florent <florent.butery@isen-ouest.yncrea.fr>
2025-02-22 10:13:42 -0500

    Enhance UI
    Center the entire screen content
    Use Card for the Question Composable
    Update Choices to use no variables to obtain almost same sized button using weight
    Add a canOther parameter to Choices for the Other Choice
    Add the question count LinearProgressIndicator to the TopAppBar

--- app/src/main/java/fr/enchantuer/sensorquiz/ui/QuestionScreen.kt
+++ app/src/main/java/fr/enchantuer/sensorquiz/ui/QuestionScreen.kt
@@ -2,6 +2,7 @@ package fr.enchantuer.sensorquiz.ui
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
@@ -12,20 +13,15 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import fr.enchantuer.sensorquiz.R
 
@@ -33,18 +29,23 @@ import fr.enchantuer.sensorquiz.R
 fun QuestionScreen(
     modifier: Modifier = Modifier,
 ) {
-    Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Question(
-            question = "placeholder",
-            imageId = R.drawable.ic_launcher_foreground,
-            modifier = Modifier.padding(bottom = 32.dp)
-        )
-        Choices(
-            choices = listOf("AAAAAAAAAAAAAAAAAAA", "BBBBBB", "Autre")
-        )
+    // Centrer le contenu verticalement et horizontalement
+    Box(modifier = modifier) {
+        Column(
+            modifier = Modifier.align(Alignment.Center),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Question(
+                question = "Quel est la capital de la France ?",
+                imageId = R.drawable.ic_launcher_foreground,
+                modifier = Modifier.padding(bottom = 32.dp)
+            )
+            Choices(
+                choices = Pair("Nantes", "Paris"),
+                canOther = true
+            )
+        }
     }
 }
 
@@ -54,57 +55,62 @@ fun Question(
     imageId: Int,
     modifier: Modifier = Modifier,
 ) {
-    Column(
-        modifier = modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Image(
-            painter = painterResource(id = imageId),
-            contentDescription = "",
-        )
-        Text(
-            textAlign = TextAlign.Center,
-            text = question,
-        )
+    Card (modifier = modifier) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = imageId),
+                contentDescription = "",
+            )
+            Text(
+                modifier = Modifier.padding(16.dp),
+                textAlign = TextAlign.Center,
+                text = question,
+            )
+        }
     }
 }
 
 @Composable
 fun Choices(
-    choices: List<String>,
+    choices: Pair<String, String>,
+    canOther: Boolean,
     modifier: Modifier = Modifier,
 ) {
-    var buttonSize by remember { mutableStateOf(IntSize.Zero) }
-
     Column(
-        modifier = modifier
-            .padding(horizontal = 16.dp),
+        modifier = modifier.padding(horizontal = 16.dp)
+            .height(IntrinsicSize.Min),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Row(
             modifier = Modifier
-                .height(IntrinsicSize.Min)
-                .fillMaxWidth(),
+                .height(IntrinsicSize.Max)
+                .weight(1f), // Aligne la hauteur minimale des boutons A et B
             horizontalArrangement = Arrangement.Center
         ) {
-            ChoiceButton(choices[0], modifier = Modifier
-                .weight(1f)
-                .fillMaxHeight()
-                .onGloballyPositioned { layoutCoordinates ->
-                    buttonSize = layoutCoordinates.size
-                }
+            ChoiceButton(
+                choices.first,
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight() // Prend la hauteur du plus grand bouton
             )
             Spacer(modifier = Modifier.width(16.dp))
-            ChoiceButton(choices[1], modifier = Modifier
-                .fillMaxHeight()
-                .weight(1f)
+            ChoiceButton(
+                choices.second,
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
             )
         }
-        if (choices.size > 2) {
+
+        if (canOther) {
             Spacer(modifier = Modifier.height(16.dp))
-            ChoiceButton(choices[2], modifier = Modifier
-                .width(with(LocalDensity.current) { buttonSize.width.toDp() })
-                .height(with(LocalDensity.current) { buttonSize.height.toDp() })
+            ChoiceButton(
+                stringResource(R.string.other),
+                modifier = Modifier
+                    .fillMaxWidth(0.5f) // Même largeur approximatif que A et B
+                    .weight(1f) // Prend la même hauteur que A et B
             )
         }
     }
@@ -123,8 +129,8 @@ fun ChoiceButton(
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun QuestionScreenPreview() {
-    QuestionScreen(modifier = Modifier.fillMaxHeight())
+    QuestionScreen()
 }
